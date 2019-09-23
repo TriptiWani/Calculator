91842b68665a2a09ee1408b12e8e907edffec351
Tripti <tripti.895@gmail.com>
2019-09-23 17:05:52 +1000

    Created a new project calculator with dotnet webapi

---
+++ Controllers/ArrayCalcController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArrayCalcController : ControllerBase
+    {
+        // GET api/arraycalc
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/arraycalc
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Reverse()
+        {
+            return new string[] { "Rvalue1", "Rvalue2" };
+        }
+
+        // GET api/arraycalc/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }        
+    }
+}
