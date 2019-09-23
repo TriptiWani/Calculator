22632064266f826ea2d49d0cddcd07aedb6645a6
Tripti <tripti.895@gmail.com>
2019-09-24 09:17:22 +1000

    Added comments wherever neccessary.

--- Domain/Repositories/ProductRepository.cs
+++ Domain/Repositories/ProductRepository.cs
@@ -8,10 +8,20 @@ namespace calculator.Domain.Repositories
 {
     public class ProductRepository : IProductRepository
     {
+        /// <summary>
+        /// Reverse the array using array manipulations only
+        /// o	Condition
+        /// 	Implement with pure array manipulation
+        // 	Don’t use Array.reverse() method
+        // 	Don’t use Linq
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
         public int[] Reverse(int[] productId)
         {
             int[] reversedArray = new int[productId.Length];
 
+            //Traversing the array and adding to the reversedArray.
             for (int i = 0; i <= productId.Length - 1; i++)
             {
                 reversedArray[i] = productId[productId.Length - i - 1];
@@ -20,14 +30,26 @@ namespace calculator.Domain.Repositories
             return reversedArray;
         }
 
+        /// <summary>
+        /// Delete the integer present at the position mentioned using pure array manipulation
+        /// o	Condition
+        // 	Implement with pure array manipulation
+        // 	Don’t use RemoveAt
+        // 	Don’t use Linq
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="productId"></param>
+        /// <returns></returns>
         public int[] DeletePart(int position, int[] productId)
         {
             try
             {
                 int[] responseArray = new int[productId.Length - 1];
+                //Index to add the integer to the response Array
                 int productIdIndex = 0;
                 for (int i = 0; i < productId.Length; i++)
                 {
+                    //if the input array index does not match the position, add to the response array.
                     if (i != (position - 1))
                     {
                         responseArray[productIdIndex] = productId[i];
@@ -40,6 +62,7 @@ namespace calculator.Domain.Repositories
             }
             catch (Exception ex)
             {
+                //catch exceptions if any.
                 Console.WriteLine("Exception at: " + ex.StackTrace);
                 return null;
             }
